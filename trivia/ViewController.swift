d72075f5a07d1901eeec25c4ca43ded8c57f5669
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-08-23 16:56:11 -0400

    added all connections to swift file

--- trivia/ViewController.swift
+++ trivia/ViewController.swift
@@ -9,11 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - UI Labels
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var choiceOne: UIButton!
+    @IBOutlet weak var choiceTwo: UIButton!
+    @IBOutlet weak var choiceThree: UIButton!
+    @IBOutlet weak var choiceFour: UIButton!
+    
+    //MARK - Properties
+    
+    
+    
+    //MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    //MARK: - Actions
+    
+    @IBAction func choiceOneTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func choiceTwoTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func choiceThreeTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func choiceFourTapped(_ sender: UIButton) {
+    }
+    
+    //MARK: - Methods
     
 
 }
