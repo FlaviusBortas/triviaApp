176a917520ae969f3824c27db6c10cab1ccdb216
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-08-23 16:09:11 -0400

    first commit

---
+++ trivia/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  trivia
+//
+//  Created by Flavius Bortas on 8/23/18.
+//  Copyright © 2018 Flavius Bortas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello")
+
+    }
+    
+
+}
+
